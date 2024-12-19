a68f6a1db0836a7f2997b4ee44504f6f5463c57e
Bashanta <ibo.codehub@gmail.com>
2024-12-20 00:12:59 +0530

    Add band name generator project

---
+++ band_name_generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")  # This is the print function that shows a welcome message
+
+city = input("Which city did you grow up in? ")  # This asks the user for the city they grew up in and stores it in 'city'
+
+pet = input("What's your pet's name? ")  # This asks the user for their pet's name and stores it in 'pet'
+
+print("Your band name could be: {} {}".format(city, pet))  # This prints the band name by combining 'city' and 'pet'
